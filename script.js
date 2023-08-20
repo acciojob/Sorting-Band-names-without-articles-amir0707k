157439bc18e253068c36c0f999d7315cff950d4b
amir0707k <34520003+amir0707k@users.noreply.github.com>
2023-08-20 14:25:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,21 +6,19 @@ let bands = [
   ];
 
 // Define the strip function to remove common words from band names
-function strip(word) {
-const regex = /^(a |the |an )/i;
-return word.replace(regex, '').trim();
+function strip (word) {
+    let regex = new RegExp('^(a |the |an )', 'i')
+    return word.replace(regex, '').trim()
 }
 
-// Sort the bands array using the strip function to ignore common words
-const sortedBands = bands.sort((a, b) => (strip(a) > strip(b)) ? 1 : -1);
+const sortedBands = bands.sort((a, b) => (strip(a) > strip(b)) ? 1 : -1)
 
-// Update the DOM with the sorted band names
-const bandList = document.getElementById('band');
-sortedBands.forEach(band => {
-const listItem = document.createElement('li');
-listItem.textContent = band;
-bandList.appendChild(listItem);
-});
+
+/** 
+ * add ul list in HTML
+ **/
+document.querySelector('#bands').innerHTML = 
+    sortedBands.map(item => `<li>${item}</li>`).join('')
 //   let newArray =[];
 
 //   for (let i = 0; i < touristSpots.length; i++) {
