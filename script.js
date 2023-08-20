8c3b07c5bf6c4599376c5f90ff22d4b91c460e7c
amir0707k <34520003+amir0707k@users.noreply.github.com>
2023-08-20 14:15:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
 //your code here
+      let touristSpots = [
+        "The Virupaksha Temple",
+        "Victoria Memorial",
+        "Tajmahal",
+      ];
+      let newArray =[];
 
+      for (let i = 0; i < touristSpots.length; i++) {
+
+        const newText = touristSpots[i].replace(/\b(?:An|The|A)\s/i, "");
+        let spotsObj = {
+           [newText] : touristSpots[i] 
+        };
+        newArray.push(spotsObj);
+      }
+    
+      let withArticle = newArray.map(obj => Object.keys(obj)[0]);
+      withArticle.sort();
+
+        for(let i = 0; i < withArticle.length; i++){
+            const foundObject = newArray.find(obj => Object.keys(obj)[0] === withArticle[i]);
+            touristSpots[i] = foundObject[withArticle[i]];
+        }
